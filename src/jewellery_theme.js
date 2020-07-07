68ac79da17f6e4a9aa0575614d07e16559a597b3
Your Name <jayasoorya@crayond.co>
2020-07-07 09:22:08 +0530

    updated fontfamily

--- src/jewellery_theme.js
+++ src/jewellery_theme.js
@@ -8,7 +8,7 @@ const NotoSerif = {
   src: `
     local('notoSerif-regular'),
     local('notoSerif-regular'),
-    url(${NotoSerifRegular}) format('ttf')
+    url('https://styloriimages-staging.s3.ap-south-1.amazonaws.com/nac_fonts/NotoSerif-Regular.ttf') format('ttf')
   `,
   unicodeRange:
     'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
@@ -50,7 +50,7 @@ export const jewelleryTheme = {
     "typography": {
       "fontFamily": 'notoSerif-regular, Arial',
     },
-    overrides: {
+    overrides: { 
       MuiCssBaseline: {
         '@global': {
           '@font-face': [NotoSerif],
