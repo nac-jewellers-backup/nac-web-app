c4253c0a132117008d170b1f2ff4519e2345ca71
Samir <samirsaji13@gmail.com>
2021-05-18 19:27:06 +0530

    header scroll item fixed and merged kalanji

--- build/precache-manifest.72167d7f1843cd74c30ae5c01662e888.js
+++ build/precache-manifest.72167d7f1843cd74c30ae5c01662e888.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "7bc7edd0cc043003a6e0",
+    "revision": "db18e81375508e39a30c",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "7bc7edd0cc043003a6e0",
+    "revision": "db18e81375508e39a30c",
     "url": "/static/js/3.js"
   },
   {
