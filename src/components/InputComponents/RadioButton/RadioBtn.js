358706bf37535396469dae0e7621e46b32dc6f9b
Renals027 <arenals95@gmail.com>
2021-05-26 23:50:12 +0530

    filter and sortby changes mbl view

--- src/components/InputComponents/RadioButton/RadioBtn.js
+++ src/components/InputComponents/RadioButton/RadioBtn.js
@@ -5,6 +5,7 @@ import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormControl from "@material-ui/core/FormControl";
 import FormHelperText from "@material-ui/core/FormHelperText";
+import Button from "@material-ui/core/Button";
 import "./index.css";
 
 const useStyles = makeStyles((theme) => ({
@@ -14,14 +15,15 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
   },
   formControl: {
-    margin: theme.spacing(0.8),
+    margin: "14px",
     fontFamily: "notoSerif-regular",
   },
   fromControlTwo: {
     fill: "#394578",
     "& span": {
-      fontSize: "0.9rem !important",
+      fontSize: "12px !important",
       fill: "#394578",
+      fontWeight:600,
       "& svg": {
         width: "0.99rem",
         fill: "#394578",
@@ -42,6 +44,12 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: "#9e9e9e40",
     },
   },
+  clearBtn:{
+    padding:"2px 7px"
+  },
+  colorMain: {
+    color: theme.palette.primary.main
+  },
 }));
 
 export default function RadioBtn(props) {
@@ -61,12 +69,57 @@ export default function RadioBtn(props) {
   // console.info('objectvaluesobjectvalues', props.values.values)
   return (
     <div
-      style={{ alignItems: "flex-end", padding: "6px" }}
+      style={{ alignItems: "flex-end"}}
       className="classSort"
     >
+             <div
+              style={{
+                padding: "16px 22px 10px",
+                borderBottom: "1px solid #e3e3e3",
+                display: "block",
+                position: "sticky",
+                top: "0px",
+              }}
+              className={`${classes.colorMain}`}
+            >
+                <i 
+                  // onClick={this.handleDrawerCloseMobile}
+                  className={`fa fa-times ${classes.colorMain}`}
+                  style={{ color: "#20205A",}}
+                ></i>
+              <button 
+                style={{
+                  background: "none",
+                  border: "none",
+                  fontWeight: "600",
+                  color: "#20205A",
+                  padding: "6px 8px",
+                  fontFamily: "notoSerif-regular",
+                }}
+              >
+                 FILTER
+              </button>
+              <Button
+                // onClick={this.handleClearAllData}
+                style={{
+                  float: "right",
+                  border: "1px solid #c0bcbc",
+                  lineHeight: "15px",
+                  fontSize: "12px",
+                  color: "#20205A",
+                  borderRadius:"0px",
+                  marginTop:"3px"
+                }}
+                className={`${classes.colorMain}`}
+                classes={{text:classes.clearBtn}}
+              >
+                Clear All
+              </Button>
+            </div>
       <FormControl
         component="fieldset"
         className={`${classes.fromControlTwo} ${classes.formControl}`}
+        style={{top:"-165px"}}
       >
         <RadioGroup
           aria-label="Gender"
