58fc36ff0b8c0e1da747e169ff8a7c64abf253f9
prem2201 <prem@crayond.co>
2021-10-07 16:03:01 +0530

    designing orderpage for mobile1

--- src/screens/Checkout/paymentOption/paymendResposeSuccess.js
+++ src/screens/Checkout/paymentOption/paymendResposeSuccess.js
@@ -80,6 +80,9 @@ class PaymentResponseSuccess extends React.Component {
     let gut_lg = localStorage.getItem("gut_lg")
       ? JSON.parse(localStorage.getItem("gut_lg"))
       : {};
+    let email = localStorage.getItem("email")
+      ? localStorage.getItem("email")
+      : "";
 
     return (
       <>
@@ -104,9 +107,9 @@ class PaymentResponseSuccess extends React.Component {
                   }}
                 >
                   <Hidden smDown>
-                    Welcome to the NAC family! We’ll take it from here.
+                    Welcome to the <b>NAC</b> family! We’ll take it from here.
                     <br />
-                    We’ve sent a confirmation email to abc@gmail.com.
+                    We’ve sent a confirmation email to <b>{email}</b>.
                   </Hidden>
                 </Typography>
               </Box>
@@ -126,9 +129,9 @@ class PaymentResponseSuccess extends React.Component {
               }}
             >
               <Hidden mdUp>
-                Welcome to the NAC family! We’ll take it from here.
+                Welcome to the <b>NAC</b> family! We’ll take it from here.
                 <br />
-                We’ve sent a confirmation email to abc@gmail.com.
+                We’ve sent a confirmation email to <b>{email}</b>.
               </Hidden>
             </Typography>
           </Grid>
