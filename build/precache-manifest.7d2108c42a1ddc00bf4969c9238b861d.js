5d5eeced1c01a5d2bd079ffc9570e2d96d694b08
prem2201 <prem@crayond.co>
2021-10-21 17:27:08 +0530

    correction in pdpage

--- build/precache-manifest.7d2108c42a1ddc00bf4969c9238b861d.js
+++ build/precache-manifest.7d2108c42a1ddc00bf4969c9238b861d.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "7fde697ddc6163595346",
+    "revision": "af7c56f624a8f8bc6d05",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "7fde697ddc6163595346",
+    "revision": "af7c56f624a8f8bc6d05",
     "url": "/static/js/3.js"
   },
   {
