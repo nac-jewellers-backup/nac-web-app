9e8c50ca5ad00479c7e3868ae62a306df0870619
prem2201 <prem@crayond.co>
2021-12-23 16:04:30 +0530

    push ui font size correction

--- build/precache-manifest.c2b7511f510092f2c39d0060f062125c.js
+++ build/precache-manifest.c2b7511f510092f2c39d0060f062125c.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "75b254816429c75c1034",
+    "revision": "64ccf100aef9fa5690b0",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "75b254816429c75c1034",
+    "revision": "64ccf100aef9fa5690b0",
     "url": "/static/js/3.js"
   },
   {
