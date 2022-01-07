8dfea2bee3b20f8cd7c59691260f1e42d0280bb1
prem2201 <prem@crayond.co>
2022-01-07 17:32:59 +0530

    completed ui correction successpage

--- build/precache-manifest.074188148067cb22925c9b824af58cca.js
+++ build/precache-manifest.074188148067cb22925c9b824af58cca.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "b1df73f57e1dfd4be2be",
+    "revision": "e1a22227b0c8a99170f1",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "b1df73f57e1dfd4be2be",
+    "revision": "e1a22227b0c8a99170f1",
     "url": "/static/js/3.js"
   },
   {
