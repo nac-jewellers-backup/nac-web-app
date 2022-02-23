f731705712143fde5f61489b7363984c40315024
vardhman <vardhman@crayond.co>
2022-02-23 21:31:42 +0530

    fixed SEO url

--- build/precache-manifest.f21e1839f9253a6391e0c553a1bcf8e4.js
+++ build/precache-manifest.f21e1839f9253a6391e0c553a1bcf8e4.js
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/3.css"
   },
   {
-    "revision": "45030434cb169257b99a",
+    "revision": "7fe71c64422fd83e6f64",
     "url": "/static/css/main.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.js"
   },
   {
-    "revision": "45030434cb169257b99a",
+    "revision": "7fe71c64422fd83e6f64",
     "url": "/static/js/main.js"
   },
   {
