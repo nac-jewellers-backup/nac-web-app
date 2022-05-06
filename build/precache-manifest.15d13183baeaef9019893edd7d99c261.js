f7fc1ec9abbcae49708f28027950d8ba3bc515c9
robo <samir@crayond.co>
2022-05-06 19:27:24 +0530

    akshayatriti page featured product

--- build/precache-manifest.15d13183baeaef9019893edd7d99c261.js
+++ build/precache-manifest.15d13183baeaef9019893edd7d99c261.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "df896155dbc6a604e500",
+    "revision": "e457115d1558e63bb718",
     "url": "/static/css/4.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.js"
   },
   {
-    "revision": "df896155dbc6a604e500",
+    "revision": "e457115d1558e63bb718",
     "url": "/static/js/4.js"
   },
   {
