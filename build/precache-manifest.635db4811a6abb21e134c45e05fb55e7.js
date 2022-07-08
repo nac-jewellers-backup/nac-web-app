60e6c1dd42a33308ccae34c9f7b756f3e746856d
vishnuvarthan <vishnuvarthan@crayond.co>
2022-07-08 10:29:11 +0530

    Id Changes

--- build/precache-manifest.635db4811a6abb21e134c45e05fb55e7.js
+++ build/precache-manifest.635db4811a6abb21e134c45e05fb55e7.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "a39e1a9e502e3a7c9439",
+    "revision": "de6c17b0ce226729e479",
     "url": "/static/css/4.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.js"
   },
   {
-    "revision": "a39e1a9e502e3a7c9439",
+    "revision": "de6c17b0ce226729e479",
     "url": "/static/js/4.js"
   },
   {
