93c5758c57f2255edb6d50a6f00cfa8cb6303efb
Vignesh1417 <vigneshwaran@crayond.co>
2022-11-03 17:41:30 +0530

    product care page inprogress

--- src/components/productCareComp/style.js
+++ src/components/productCareComp/style.js
@@ -1,5 +1,61 @@
 import { makeStyles } from "@material-ui/core/styles";
 const ProductCareStyles = makeStyles((theme) => ({
-  
+  contentText: {
+    padding: "0 110px !important",
+    boxShadow: "none !important",
+    margin: "0px 0px !important",
+    [theme.breakpoints.down("md")]: {
+      padding: "0 50px !important",
+    },
+    [theme.breakpoints.down("xs")]: {
+      padding: "0 10px !important",
+    },
+  },
+
+  main: {
+    margin: "25px 0",
+  },
+
+  cardBox: {
+    marginBottom: "35px",
+    boxShadow: "11px 10px 14px -10px rgba(0,0,0,0.76)",
+    padding: "20px 25px",
+    backgroundColor: "#f0f0f0",
+    borderRadius: "0px !important",
+  },
+
+  image: {
+    width: "100%",
+    "& img": {
+      width: "100%",
+    },
+  },
+
+  heading: {
+    marginBottom: "25px",
+    "& p": {
+      fontSize: "25px",
+      color: "#2a2f70",
+      fontFamily: "notoSerif-bold !important",
+      fontStyle: "italic",
+    },
+  },
+
+  content: {
+    "& ul": {
+      paddingLeft: "22px",
+      "& li": {
+        "&::marker": {
+          color: "#B78231",
+        },
+        "& p": {
+          fontSize: "18px",
+          color: "#86878a",
+          fontFamily: "notoSerif-regular !important",
+          fontWeight: 540,
+        },
+      },
+    },
+  },
 }));
 export default ProductCareStyles;
--- src/mappers/dummydata/productCare.js
+++ src/mappers/dummydata/productCare.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import pic1 from "../../assets/pic_1.jpg";
 import pic2 from "../../assets/pic_2.jpg";
 import pic3 from "../../assets/pic_3.jpg";
@@ -11,9 +13,18 @@ export const productCareData = [
     heading: "The guide to caring for gold",
     content: [
       "To clean gold jewellery, mix a few drops of mild dish detergent with warm water and scrub it with a soft cloth. Rinse with warm water and dry it off.",
-      "Toothpaste can be used as a cleansing agent but it is advisable to use gentler washing fluids instead. Soak your jewellery in warm water for two minutes and then gently clean it with a soft cloth. Rinse and dry for the final touch.",
+      <div style={{ marginTop: "25px", marginBottom: "25px" }}>
+        Toothpaste can be used as a cleansing agent but it is advisable to use
+        gentler washing fluids instead. Soak your jewellery in warm water for
+        two minutes and then gently clean it with a soft cloth. Rinse and dry
+        for the final touch.
+      </div>,
       "Ammonia is another alternative but it's best to use it only occasionally as it is a strong cleaning agent. Mix four parts of warm water and one part of household ammonia, submerge the ornaments for not more than ten minutes before rinsing and drying them.",
-      "Gold Jewellery with gemstones should not be brushed hard or rubbed repeatedly as this may loosen the gemstones. Clean it with a soft cloth dipped in a solution of mild dish detergent and warm water instead.",
+      <div style={{ marginTop: "25px", marginBottom: "25px" }}>
+        Gold Jewellery with gemstones should not be brushed hard or rubbed
+        repeatedly as this may loosen the gemstones. Clean it with a soft cloth
+        dipped in a solution of mild dish detergent and warm water instead.
+      </div>,
       "Remember that moisture speeds the tarnishing process so it's important to make sure that your jewellery is dry before putting it away.",
     ],
   },
@@ -22,9 +33,17 @@ export const productCareData = [
     heading: "The guide to caring for silver",
     content: [
       "The most commonly used method to clean silver is to use a specialized cleaning solution and cloth. There are specifically designed to clean silver and the cleaning cloth is soft and often infused with  anti-tarnishing agents.",
-      "Another alternative is to buff the jewellery with a soft cloth after washing it in a bowl of lukewarm water to. Add a few drops of dishwashing liquid to help remove tarnishing.",
+      <div style={{ marginTop: "25px", marginBottom: "25px" }}>
+        Another alternative is to buff the jewellery with a soft cloth after
+        washing it in a bowl of lukewarm water to. Add a few drops of
+        dishwashing liquid to help remove tarnishing.
+      </div>,
       "For heavier tarnish, make a paste with three parts baking soda to one part water. Wet the silver and apply the paste with a soft cloth. Rinse with cool water and buff with a until dry. Do not soak silver as this speeds up the tarnishing process.",
-      "Prompt cleaning is important when silver has been exposed to certain food containing sulphur or those thatare acidic. Food such are salt, eggs, onions, mayonnaise and vinegar are harmful to silver.",
+      <div style={{ marginTop: "25px" }}>
+        Prompt cleaning is important when silver has been exposed to certain
+        food containing sulphur or those thatare acidic. Food such are salt,
+        eggs, onions, mayonnaise and vinegar are harmful to silver.
+      </div>,
     ],
   },
   {
@@ -32,7 +51,12 @@ export const productCareData = [
     heading: "The guide to caring for platinum",
     content: [
       "Famed for its resilient and non-oxidising qualities, Platinum requires regular maintenance.",
-      "Just as with gold, simply soak the jewellery in water with mild detergent and then rub off the dirt, grime and even the fingerprints which eat away the natural shine. Rinse and dry to get the original sparkle back on your platinum jewellery.",
+      <div style={{ marginTop: "25px", marginBottom: "25px" }}>
+        Just as with gold, simply soak the jewellery in water with mild
+        detergent and then rub off the dirt, grime and even the fingerprints
+        which eat away the natural shine. Rinse and dry to get the original
+        sparkle back on your platinum jewellery.
+      </div>,
       "Platinum pieces adorned with stones should NOT be soaked in water. Just clean it with water, rinse and dry it. A final cleaning with a lint cloth or any other soft cloth should bring back the glitter.",
     ],
   },
@@ -41,7 +65,11 @@ export const productCareData = [
     heading: "The guide to caring for solitaires",
     content: [
       "The shine and the sparkle of your solitaire jewellery can be lost to surface scratches and dust. It is always better to soak the jewellery in soapy water so that the settings don’t dislodge.",
-      "A simple buff with a soft cloth after a mild soap wash can actually bring the lustre back. It is also recommended that you don’t expose it to any kind of chemicals or oil for a long time.",
+      <div style={{ marginTop: "25px", marginBottom: "25px" }}>
+        A simple buff with a soft cloth after a mild soap wash can actually
+        bring the lustre back. It is also recommended that you don’t expose it
+        to any kind of chemicals or oil for a long time.
+      </div>,
       "Regular cleaning, twice a week will help you to get rid of the dirt tucked into the crevices.",
     ],
   },
@@ -50,9 +78,14 @@ export const productCareData = [
     heading: "The guide to caring for diamonds",
     content: [
       "The care for loose diamonds is easier than the ones set in jewellery as the fear of dislodging doesn’t arise.",
-      "Asimple soapy wash followed by a pat dray and a soft cloth buff should do the trick.",
+      <div style={{ marginTop: "25px", marginBottom: "25px" }}>
+        Asimple soapy wash followed by a pat dray and a soft cloth buff should
+        do the trick.
+      </div>,
       "It is, however, necessary to wrap loose diamonds carefully in a soft cloth individually as putting it together might result in scratches to the stones.",
-      "Of course, exposure to chemicals and oil deposits are a big no.",
+      <div style={{ marginTop: "25px" }}>
+        Of course, exposure to chemicals and oil deposits are a big no.
+      </div>,
     ],
   },
   {
@@ -60,9 +93,16 @@ export const productCareData = [
     heading: "The guide to caring for coloured diamonds",
     content: [
       "Fancy coloured diamonds, like regular diamonds, can be cleaned with a lint-free cloth after soaking it in a gentle soapy solution.",
-      "A few commercial cleaning solutions or household gentle washes can also be used. However, we recommended staying clear of harsher chemicals such as abrasive household cleaners.",
-      " Though diamonds are known to withstand high temperatures, any sudden and extreme change in temperatures can cause a thermal shock and can create fractures.",
-      " Cleavages, or spots where the atomic bond is at the lowest, are the most vulnerable.",
+      <div style={{ marginTop: "25px", marginBottom: "25px" }}>
+        A few commercial cleaning solutions or household gentle washes can also
+        be used. However, we recommended staying clear of harsher chemicals such
+        as abrasive household cleaners.
+      </div>,
+      "Though diamonds are known to withstand high temperatures, any sudden and extreme change in temperatures can cause a thermal shock and can create fractures.",
+      <div style={{ marginTop: "25px", marginBottom: "25px" }}>
+        Cleavages, or spots where the atomic bond is at the lowest, are the most
+        vulnerable.
+      </div>,
       "Also, constant exposure to oil and other dirt can cause your favourite coloured diamond to lose its shine and sparkle. So regular cleaning is a must.",
     ],
   },
