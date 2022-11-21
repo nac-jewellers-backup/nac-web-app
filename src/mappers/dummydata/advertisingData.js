1bd4aad7d1197524d9dd031553609ade22cc742b
Vignesh1417 <vigneshwaran@crayond.co>
2022-11-21 14:07:10 +0530

    nac advertising data created

---
+++ src/mappers/dummydata/advertisingData.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+export const AdvertisingPageData = [
+  {
+    component: "CustomNews",
+    props: {
+      cardContent: [
+        {
+          img: "https://s3-ap-southeast-1.amazonaws.com/media.nacjewellers.com/resources/static+page+images/collection+page/Group+55%402x.png",
+          description: (
+            <div>
+              NAC Jewellers The Rewind Collection{" "}
+              <span>For Antique Jewellery Line</span>
+            </div>
+          ),
+          button_text: "View",
+          url: "",
+        },
+        {
+          img: "https://s3-ap-southeast-1.amazonaws.com/media.nacjewellers.com/resources/static+page+images/collection+page/Group+68%402x.png",
+          description: (
+            <div>
+              NAC Jewellers The Breezee Collection{" "}
+              <span>For Light Weight Jewellery</span>
+            </div>
+          ),
+          button_text: "View",
+          url: "",
+        },
+        {
+          img: "https://s3-ap-southeast-1.amazonaws.com/media.nacjewellers.com/resources/static+page+images/collection+page/Group+64%402x.png",
+          description: (
+            <div>
+              NAC - Muhurtham Jewellery <span>For Bridal Collection</span>
+            </div>
+          ),
+          button_text: "View",
+          url: "",
+        },
+        {
+          img: "https://s3-ap-southeast-1.amazonaws.com/media.nacjewellers.com/resources/static+page+images/collection+page/Group+50%402x.png",
+          description: (
+            <div>
+              NAC Jewellers Diamond Ad <span>For The Diamond Collection</span>
+            </div>
+          ),
+          button_text: "View",
+          url: "",
+        },
+        {
+          img: "https://s3-ap-southeast-1.amazonaws.com/media.nacjewellers.com/resources/static+page+images/collection+page/Group+59%402x.png",
+          description: (
+            <div>
+              NAC - Breezee Light Weight Jewellery{" "}
+              <span>For Breezee Collection</span>
+            </div>
+          ),
+          button_text: "View",
+          url: "",
+        },
+        {
+          img: "https://s3-ap-southeast-1.amazonaws.com/media.nacjewellers.com/resources/static+page+images/collection+page/Group+73%402x.png",
+          description: (
+            <div>
+              NAC - Muhurtham Collection <span>For Muhurtham Collection</span>
+            </div>
+          ),
+          button_text: "View",
+          url: "",
+        },
+      ],
+    },
+  },
+];
