dd88b747764c89b58e371ff483b9b256da0ef581
vishnuvarthan <vishnuvarthan@crayond.co>
2022-12-06 17:59:37 +0530

    new build changes

---
+++ build/precache-manifest.6b157e634d61a6267123187f1a457817.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf63d3941df109b8a649beddd089251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00293e3f11172df86305",
+    "url": "/static/css/2.css"
+  },
+  {
+    "revision": "6bd06511186ee5d923d1",
+    "url": "/static/css/3.css"
+  },
+  {
+    "revision": "ea02f3511294b1882410",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "00293e3f11172df86305",
+    "url": "/static/js/2.js"
+  },
+  {
+    "revision": "6bd06511186ee5d923d1",
+    "url": "/static/js/3.js"
+  },
+  {
+    "revision": "b9e7cb97eb0c9f389473",
+    "url": "/static/js/4.js"
+  },
+  {
+    "revision": "ea02f3511294b1882410",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "67501ddb9e15004a2ab4",
+    "url": "/static/js/runtime~main.js"
+  },
+  {
+    "revision": "88cd9faca27dc0dfb87c322b97069a14",
+    "url": "/static/media/Alive_Mobile.jpg"
+  },
+  {
+    "revision": "44f37f219b69ed4fe5de52a0a3fe2fb8",
+    "url": "/static/media/Birth stone_mobile.jpg"
+  },
+  {
+    "revision": "eec94e2c340712c8ff34d62260a1c116",
+    "url": "/static/media/Ear_mobile.jpg"
+  },
+  {
+    "revision": "32f7bc6175ae609bff02c8263ad94be6",
+    "url": "/static/media/ExperienceBanner.jpg"
+  },
+  {
+    "revision": "42da02fcd523e88adc28ad34159098c7",
+    "url": "/static/media/Experiences banner.jpg"
+  },
+  {
+    "revision": "4b43159ab7b19a9eae97ccd819463473",
+    "url": "/static/media/Lotus_Mobile.jpg"
+  },
+  {
+    "revision": "4e2217c1a8309b2762499eb007d4109d",
+    "url": "/static/media/MyriadPro-Light.otf"
+  },
+  {
+    "revision": "7ac043ce58c2e61adcf7ebac9625d31b",
+    "url": "/static/media/MyriadPro-Regular.ttf"
+  },
+  {
+    "revision": "42ab08a78e1c852c370225fd65576061",
+    "url": "/static/media/NAC---Desktop-UI---About Us, FAQ, Privacy, T&C-10.jpg"
+  },
+  {
+    "revision": "da674f2a94672bb546311eb6d023c6d4",
+    "url": "/static/media/NAC---Desktop-UI---About Us, FAQ, Privacy, T&C-5.jpg"
+  },
+  {
+    "revision": "19f6e1085e3c7180af2afe16ef37eb50",
+    "url": "/static/media/NAC---Desktop-UI---About Us, FAQ, Privacy, T&C-6.jpg"
+  },
+  {
+    "revision": "ceff4e64a1a70449df146a39417c40fa",
+    "url": "/static/media/NAC---Desktop-UI---About Us, FAQ, Privacy, T&C-7.jpg"
+  },
+  {
+    "revision": "0b6c1cc0cff5bc7918ef10e3701f0e2c",
+    "url": "/static/media/NAC---Desktop-UI---About Us, FAQ, Privacy, T&C-8.jpg"
+  },
+  {
+    "revision": "25dcca4ead002090d96ffb05fb269de2",
+    "url": "/static/media/NAC---Desktop-UI---About Us, FAQ, Privacy, T&C-9.jpg"
+  },
+  {
+    "revision": "57a7037a4c4307321286505a75732883",
+    "url": "/static/media/NAC---Desktop-UI---NAC-Experiences (LL, EP, BS, Alive)_Sample-19.png"
+  },
+  {
+    "revision": "c714bc76aa0f401332cb5a8cfec9c7f8",
+    "url": "/static/media/NAC---Desktop-UI---NAC-Experiences (LL, EP, BS, Alive)_Sample-20.png"
+  },
+  {
+    "revision": "cb2f6c4db5e14a7459a6ff715b1e608d",
+    "url": "/static/media/NAC---Desktop-UI---NAC-Experiences (LL, EP, BS, Alive)_Sample-21.png"
+  },
+  {
+    "revision": "00e8f192c243bd4a46c67abdd5148894",
+    "url": "/static/media/NAC---Desktop-UI---NAC-Experiences (LL, EP, BS, Alive)_Sample-22.png"
+  },
+  {
+    "revision": "33f06877dc9c5640dd7f7a9e06cd7716",
+    "url": "/static/media/NAC---Desktop-UI---NAC-Experiences-(LL,-EP,-BS,-Alive)_Sample-10.jpg"
+  },
+  {
+    "revision": "6d48024b29329ed7ae2a5bddd58c17b8",
+    "url": "/static/media/NAC---Desktop-UI---NAC-Experiences-(LL,-EP,-BS,-Alive)_Sample-11.jpg"
+  },
+  {
+    "revision": "68d64785d1c312689ab376a8732fd036",
+    "url": "/static/media/NAC---Desktop-UI---NAC-Experiences-(LL,-EP,-BS,-Alive)_Sample-8.jpg"
+  },
+  {
+    "revision": "4bf2ff9abdc2e4ad90f1d94197600d6b",
+    "url": "/static/media/NAC---Desktop-UI---NAC-Experiences-(LL,-EP,-BS,-Alive)_Sample-9.jpg"
+  },
+  {
+    "revision": "1fd398950fe8652ef746111a8ae12831",
+    "url": "/static/media/career_banner.jpg"
+  },
+  {
+    "revision": "1cd6366ebcaf733d7c40ebbd7309d713",
+    "url": "/static/media/career_mobile.jpg"
+  },
+  {
+    "revision": "2b3d63f761a61e5c65a4773359c4eb40",
+    "url": "/static/media/close.svg"
+  },
+  {
+    "revision": "6b446a380e308ad3d84cf955d088f3be",
+    "url": "/static/media/icons8-facebook.svg"
+  },
+  {
+    "revision": "992ac34a80cdd509871cff78c6d98e72",
+    "url": "/static/media/icons8-gmail.svg"
+  },
+  {
+    "revision": "28cd30ea4ad92b4520d9a3fb2634a87d",
+    "url": "/static/media/img1ProductModal.jpg"
+  },
+  {
+    "revision": "b944ac0307e92dae56a16dc18b6318a1",
+    "url": "/static/media/img2ProductModal.jpg"
+  },
+  {
+    "revision": "2e0c3b28d442f07cf1fb810420137dc5",
+    "url": "/static/media/img3ProductModal.jpg"
+  },
+  {
+    "revision": "337eef9d867ffd752a548702f05cb8e6",
+    "url": "/static/media/img4ProductModal.jpg"
+  },
+  {
+    "revision": "6e25bad6f003be8fe1ddf1c68d2b4dd9",
+    "url": "/static/media/logout.svg"
+  },
+  {
+    "revision": "4d7fcda5f5b21e33916e363c7052c993",
+    "url": "/static/media/pic_1.jpg"
+  },
+  {
+    "revision": "c4f9847964c79dea638827f5172bdf02",
+    "url": "/static/media/pic_2.jpg"
+  },
+  {
+    "revision": "dbf98a5f3894b93e779f16c8b5338432",
+    "url": "/static/media/pic_3.jpg"
+  },
+  {
+    "revision": "929c2669d4593b22f5c228b5dee8d9c0",
+    "url": "/static/media/pic_4.jpg"
+  },
+  {
+    "revision": "7b460f40a1259dcddddac683d97ab3ab",
+    "url": "/static/media/pic_5.jpg"
+  },
+  {
+    "revision": "4910f7a31d113d9eab2f3b27676b294c",
+    "url": "/static/media/pic_6.jpg"
+  },
+  {
+    "revision": "57b56bb10faf3b9dbb17bc91e6be7baf",
+    "url": "/static/media/shopping.svg"
+  },
+  {
+    "revision": "b03bd5d7d745593acdc60d6c1d6591d4",
+    "url": "/static/media/silverOpenLink.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.woff"
+  },
+  {
+    "revision": "681db1b372ef45af725df7762fcb49ee",
+    "url": "/static/media/styloriGagets.png"
+  },
+  {
+    "revision": "26f4adaec0fa060fe6ef92c4c4f7c485",
+    "url": "/static/media/telephone.svg"
+  },
+  {
+    "revision": "1737e5e0a88de55dc6cf1a773e4b0816",
+    "url": "/static/media/topPicksOne.jpg"
+  },
+  {
+    "revision": "35f5ee82b7e9b5e66f70a99aefcb3865",
+    "url": "/static/media/topPicksThree.jpg"
+  },
+  {
+    "revision": "37b89e280ecc047a56d66fdb6404bb23",
+    "url": "/static/media/topPicksTwo.jpg"
+  },
+  {
+    "revision": "bcc776345852aa9836e64bcc3e000864",
+    "url": "/static/media/user-shape.svg"
+  }
+]);
